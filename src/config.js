c0ca4fb6a1dd24cf12df6a9b58695242319c5c84
lleonnssilva <leoguaruleo@gmail.com>
2019-04-24 10:10:50 -0300

    make it bettera

--- src/config.js
+++
@@ -1,7 +0,0 @@
-CONNECTION_STRING =
-  "mongodb://curitiba:curitiba123@ds127436.mlab.com:27436/dbcuritiba";
-PORT = 3000;
-SALT_KEY = "leoguaruleo";
-SENDGRID_API_KEY =
-  "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";
-TEMPLATE_EMAIL = "Olá, <strong>{0}</strong>, seja bem vindo à Node Store!";
